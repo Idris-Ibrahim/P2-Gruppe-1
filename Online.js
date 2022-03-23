488dfbf7a096af888831df73c78731777aa54a7d
Casper2910 <casperbramm@hotmail.com>
2022-03-23 13:14:11 +0100

    Updated Online

--- Online.js
+++ Online.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql2');
-//Port difined as a variable
-const port = 3000;
+
+//connection to backend server
 const connection = mysql.createConnection({
     host: "10.92.0.104",
     user: "remote",
@@ -15,3 +15,13 @@ connection.connect(function(err){
     if (err) throw err;
     console.log('CONNECTED');
 });
+
+//app listed on port defined earlier
+/*app.listen(connection, function(){
+    console.log(`port open at Localhost:${port}`);
+});*/
+
+//Bruger object til at sende query til Databasen
+connection.query('SELECT * FROM customers;', (err, res)=>{
+        console.log(res);    
+    });
