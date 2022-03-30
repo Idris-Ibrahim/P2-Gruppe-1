80fee2a4eb5e461a440f94c1cdbe4e9bf4c5835b
Casper2910 <casperbramm@hotmail.com>
2022-03-30 13:48:29 +0200

    fddf

--- Ez_Eventman_69/Sequelize/Sequelize.js
+++ Ez_Eventman_69/Sequelize/Sequelize.js
@@ -1,8 +1,8 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize("Eventman", "root", "P2grupper#mote",{
+const connection = new Sequelize("Eventman", "remote", "P2grupper#mote",{
     dialect: "mysql",
-    host: "localhost",
+    host: "10.92.0.104",
 });
 
 module.exports = connection;
