aa95f515bafa46e09a1909bdcb9a443376c52e6c
Casper2910 <casperbramm@hotmail.com>
2022-04-01 10:46:48 +0200

    Events.hasMany(orgname)

--- Ez_Eventman_69/Sequelize/viewSQL.js
+++ Ez_Eventman_69/Sequelize/viewSQL.js
@@ -1,6 +1,10 @@
 const sequelize = require("./Sequelize");
 
+
 const Events = require("./EventmanSequlize");
+
+//koden under kan danne en pathway fra orgname til organitations tabled.
+//Events.hasMany(Orgname);
 
 //force erstatter forige data:
 sequelize
@@ -9,8 +13,10 @@ sequelize
     //dato skal være: (år-måned-dag):
     Events.create({orgname:"Scrumklubben",event_name: "Scrummespil",lokation: "Scrumkammeret på AAU",tid: "13:34",dato:"2023-04-14"});
     console.log(result);
+
 })
 
+//catcher en error hvis der opstår en
 .catch ((err) =>{
     console.log(err);
 });
