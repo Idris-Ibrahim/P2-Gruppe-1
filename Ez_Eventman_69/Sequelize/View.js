b264ced7a6f133c7d3d9e17fcbc7e2f9180616ed
Casper2910 <casperbramm@hotmail.com>
2022-04-01 11:40:26 +0200

    eded

--- Ez_Eventman_69/Sequelize/View.js
+++ Ez_Eventman_69/Sequelize/View.js
@@ -5,8 +5,11 @@ const Events = require("./EventmanSequlize");
 sequelize
 .sync()
 .then(Events =>{
-    const viewEvents = await Events.findALL();
-    console.log(viewEvents.every(viewEvent => viewEvent instanceof Events));
-    console.log("All users:", JSON.stringify(users, null, 2));
+    return Events.findAll({where: dato < 0});
+
+})
+.then(Events =>{
+  console.log(Events)
+
 });
 
