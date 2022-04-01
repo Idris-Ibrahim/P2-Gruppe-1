84bf069cb7e0f345f5d96ea0480ebed67340aad6
Casper2910 <casperbramm@hotmail.com>
2022-04-01 14:21:14 +0200

    Sequelize with Op

--- Ez_Eventman_69/Sequelize/View.js
+++ Ez_Eventman_69/Sequelize/View.js
@@ -1,10 +1,12 @@
 const sequelize = require("./SequelizeDB");
-
+const Sequelize = require("sequelize");
+//Op operator functionality
+const {Datatypes, Op} = Sequelize;
 const Events = require("./EventmanSequlize");
 
 sequelize
 Events.sync({ alter: true }).then(()  => {
-    return Events.findAll({});
+    return Events.findAll({ where: { id: {[Op.gt]: 2}}});
 }).then((data) => {
     data.forEach((element) => {
         console.log(element.toJSON());
