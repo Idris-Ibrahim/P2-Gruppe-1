9b250d9d3f6dcdf7b6941f93333d796d8cffea50
Casper2910 <casperbramm@hotmail.com>
2022-04-07 11:46:26 +0200

    functions is now =>

--- Ez_Eventman_69/Sequelize/Insert.js
+++ Ez_Eventman_69/Sequelize/Insert.js
@@ -8,13 +8,13 @@ const Events = require("./EventmanSequlize");
 //force erstatter forige data:
 sequelize
 .sync(/*{force:true}*/)
-.then((result)=>{
+.then((result) => {
     //dato skal være: (år-måned-dag):
     Events.create({orgname:"Kebabklubben",event_name: "Spisning",lokation: "Kebabbiksen",tid: "17:00",dato:"2023-04-05"});
     console.log(result);
 })
 
 //catcher en error hvis der opstår en
-.catch ((err) =>{
+.catch ((err) => {
     console.log(err);
 });
