33bb6ab03487b3b290b9546a7abaf6e31bcb21cf
Casper2910 <94049624+Casper2910@users.noreply.github.com>
2022-04-13 10:16:36 +0200

    evennts

--- Ez_Eventman_69/app.js
+++ Ez_Eventman_69/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//Events objekt fra Sequelize mappen
+var Events = require('./Sequelize/EventmanSequlize');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
