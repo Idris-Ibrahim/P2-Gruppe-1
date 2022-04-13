7811b69dc6a6b5f1e06611ae22b2c3030b14c80b
Creich21 <creich21@student.aau.dk>
2022-04-13 11:00:01 +0200

    .

--- Ez_Eventman_69/Sequelize/View.js
+++ Ez_Eventman_69/Sequelize/View.js
@@ -1,10 +1,23 @@
 // Brugt denne guide https://www.youtube.com/watch?v=jWdVy265Q-A
 const Sequelize = require("sequelize");
+const async = require("async");
 //Op operator functionality
 const {Datatypes, Op} = Sequelize;
 const Events = require("./EventmanSequlize");
+//
+// Events.sync({ alter: true }).then(()  => {
+//     return Events.findAll({ order: [['dato'],['tid']]});
+// }).then((data) => {
+//     data.forEach((element) => {
+//         console.log(element.toJSON());
+//     })
+// })
+//     .catch((err) => {
+//         console.log(err)
+//     });
 
-Events.sync({ alter: true }).then(()  => {
+
+exports.view = function (req, res, next) {
     return Events.findAll({ order: [['dato'],['tid']]});
 }).then((data) => {
     data.forEach((element) => {
@@ -14,3 +27,4 @@ Events.sync({ alter: true }).then(()  => {
     .catch((err) => {
         console.log(err)
     });
+}
