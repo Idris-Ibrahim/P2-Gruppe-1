6a9596e728922482cc4ec6895ce43d59c82b76ab
Creich21 <creich21@student.aau.dk>
2022-04-20 13:53:47 +0200

    fixed main  pug reference

--- amped/controllers/maincontroller.js
+++ amped/controllers/maincontroller.js
@@ -5,7 +5,7 @@ const Main = require("../models/main");
 exports.viewintro = (req, res, next) => {
     return Main.findAll()
         .then((data) => {
-            res.render('Main', {mainlist: data });
+            res.render('main', {mainlist: data });
         })
         .catch((err) => {
             console.log(err)
