8fcb1a3ef7e709ee09e4f186d7d3a778b48e40c2
Idris-Ibrahim <93320320+Idris-Ibrahim@users.noreply.github.com>
2022-04-22 14:34:02 +0200

    sort completely done

--- amped/routes/main.js
+++ amped/routes/main.js
@@ -13,7 +13,16 @@ router.get('/', events_controller.viewevents);
 // gets all sorted events from events_controller
 router.get('/events', events_controller.viewevents);
 
-router.get('/events/sortddateesc', events_controller.eventsdesc);
+router.get('/events/sortddateasc', events_controller.viewevents);
+
+// gets all event s sorted DESC
+router.get('/events/sortddatedesc', events_controller.eventsdesc);
+
+// gets all sorted events from events_controller
+router.get('/events/eventnameasc', events_controller.eventnameasc);
+
+// gets all sorted events from events_controller
+router.get('/events/eventnamedesc', events_controller.eventsdesc);
 
 /// Groups
 // gets all groups from groups_controller
