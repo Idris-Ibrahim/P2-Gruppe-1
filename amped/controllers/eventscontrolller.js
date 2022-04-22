0fa8de1c62be8b2483770ff0269fd0beeb1d45a5
Casper2910 <casperbramm@hotmail.com>
2022-04-22 11:07:53 +0200

    k

--- amped/controllers/eventscontrolller.js
+++ amped/controllers/eventscontrolller.js
@@ -3,8 +3,21 @@ const {Datatypes, Op} = Sequelize;
 const Events = require("../models/events");
 
 // all events sorted by date and time
+/*
 exports.viewevents =  function (req, res, next) {
     return Events.findAll({ order: [['dato'],['tid']]})
+        .then(function(data) {
+            res.render('events', {eventlist: data });
+        })
+        .catch( function(err)  {
+            console.log(err)
+        });
+}
+*/
+
+exports.viewevents =  function (req, res, next) {
+    return Events.findAll
+    ({ order: [['dato'],['tid']][Sequelize.fn(date_format, Sequelize.col('date_col'), '%d-%m-%y')]})
         .then(function(data) {
             res.render('events', {eventlist: data });
         })
