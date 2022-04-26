ff8058e073b172e8e3c48e42124df0b9edbf76a1
Casper2910 <casperbramm@hotmail.com>
2022-04-26 10:31:32 +0200

    d

--- amped/controllers/groupscontroller.js
+++ amped/controllers/groupscontroller.js
@@ -30,3 +30,20 @@ exports.registergroups = async function (req, res, next) {
     });
 }
 
+//justering af admin status:
+/*
+exports.registergroups = async function (req, res, next) {
+    await Groups.create({
+        group_name: req.body.group_name,
+        email: req.body.email,
+        password: req.body.password,
+        roles: 0
+    }).then(function (groups) {
+        if (groups) {
+            res.redirect('/login');
+        } else {
+            response.status(400).send('Error in insert new record');
+        }
+    });
+}
+*/
