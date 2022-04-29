ba0600cb3a09c68a4383e934ff05abda53ab0d83
Johannes Hansen <92086260+Joha00@users.noreply.github.com>
2022-04-29 11:54:22 +0200

    Calendar controller update

--- amped/controllers/calendarcontroller.js
+++ amped/controllers/calendarcontroller.js
@@ -1,4 +1,16 @@
-const Sequelize = require("sequelize");
-const {Datatypes, Op} = Sequelize;
-const Calender = require("../models/calander");
+const router = require("express").Router();
+const Events = require("../models/events");
 
+
+router.post("/creat-event", async (req,res) => {
+    const events = Events(req.body);
+    await event.save();
+    res.sendStatus(201);
+})
+
+router.get("/get-events", async (req,res) => {
+    const events = await Events
+})
+
+
+module.exports = router;
