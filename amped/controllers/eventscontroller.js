65cf253eda4d07aee857d6874dcc31ccbf6ead9f
Casper2910 <casperbramm@hotmail.com>
2022-05-06 13:52:15 +0200

    h

--- amped/controllers/eventscontroller.js
+++ amped/controllers/eventscontroller.js
@@ -20,7 +20,7 @@ exports.viewevents =  function (req, res, next) {
 
 exports.vieweventsforgroup =  function (req, res, next) {
     console.log(req.session)
-    return Events.findAll({order: [['dato'],['tid']]},{where: group_id = req.session.group_id})
+    return Events.findAll({where: {group_id : req.session.Group.id}},{order: [['dato'],['tid']]})
         .then(function(data) {
             res.render('grouppanel', {eventlist: data },
             console.log(data));
