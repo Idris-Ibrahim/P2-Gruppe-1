69198288980cced1d3c4522041a43a18d0b13410
Creich21 <creich21@student.aau.dk>
2022-05-10 14:52:00 +0200

    Fixed times in calendar

--- amped/controllers/calendarcontroller.js
+++ amped/controllers/calendarcontroller.js
@@ -8,14 +8,16 @@ exports.calendar =  function (req, res, next) {
     return Events.findAll({ order: [['dato'],['tid']]})
     .then(function(data) {
         const caldata = data.map(obj => {
+            const split = obj.tid.split(":");
             return {
                 title: obj.event_name,
-                start: obj.dato,
+                // start: obj.dato.setSeconds(obj.dato.getSeconds()+(3600*split[0]) + (60*split[1]) + split[2]),
+                start: obj.dato.setHours(split[0],split[1],split[2]),
                 url: '/eventsinfo/?id='+ obj.id
-
             }
         })
         res.render('calendar', {eventlist: caldata})
+        console.log(caldata);
     })
     .catch( function(err)  {
         console.log(err)
