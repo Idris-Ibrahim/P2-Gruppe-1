abad0b17a245fdfeb546b83189e429950858e19b
Casper2910 <94049624+Casper2910@users.noreply.github.com>
2022-05-20 09:45:15 +0200

    man kan ikke update til tomt name eller password mere

--- amped/controllers/groupscontroller.js
+++ amped/controllers/groupscontroller.js
@@ -76,33 +76,45 @@ exports.updateadmin = (req, res, next) => {
 // Update Group
 exports.groupsupdate = function (req, res, next) {
     const idcheck = req.body.id
+    const navn = req.body.group_name
+    const passwordcheck = req.body.password
     console.log(idcheck)
     if (req.session.loggedIn !== true || req.session.Group.roles < 2) {
         res.render("nopugmission")
         return
     }
-
-    Groups.update(
-        // Values to update
-        {
-            id:idcheck,
-            group_name: req.body.group_name,
-            group_about: req.body.group_about,
-            group_email: req.body.group_email,
-            password: req.body.password,
-            fburl: req.body.fburl,
-        },
-        {
-            where: {'id': {[Op.eq]: idcheck} }
-        }
-    ).then(function (groups) {
-        if (groups) {
-            res.redirect('/admin/groups');
-        } else {
-            response.status(400).send('Error in update');
-        }
-    });
-}
+    if (passwordcheck.length < 3){
+        res.send('password must be more than 3 characters');
+    }
+            //checks if name is already in use:
+        Groups.findAll({where : {'group_name':{ [Op.eq]: navn}}})
+            .then(function(data){
+                if (data.length == 0 && navn.length > 0){
+                    Groups.update(
+                        // Values to update
+                        {
+                            id:idcheck,
+                            group_name: navn,
+                            group_about: req.body.group_about,
+                            group_email: req.body.group_email,
+                            password: passwordcheck,
+                            fburl: req.body.fburl,
+                        },
+                        {
+                            where: {'id': {[Op.eq]: idcheck} }
+                        }
+                    ).then(function (groups) {
+                        if (groups) {
+                            res.redirect('/admin/groups');
+                        } else {
+                            response.status(400).send('Error in update');
+                        }
+                    });
+                }else{
+                    res.send('Username is either already in use or empty');
+                }
+                })
+            }
 
 //
 exports.groupinfo = function(req, res, next){
