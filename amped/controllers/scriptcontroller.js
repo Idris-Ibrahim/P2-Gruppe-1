ca13295d96c4053a9c856600f9d125b2cae288ba
Creich21 <creich21@student.aau.dk>
2022-05-23 20:26:32 +0200

    fix calendar

--- amped/controllers/scriptcontroller.js
+++ amped/controllers/scriptcontroller.js
@@ -1,15 +1,7 @@
-const Fullcalendar = require('fullcalendar');
-
 exports.fullcalendarcss =  function (req, res, next) {
         res.sendFile(__dirname+'/static_content/fullcalendar.css')
-        .catch( function(err)  {
-            console.log(err)
-        });
 }
 
 exports.fullcalendarjs =  function (req, res, next) {
         res.sendFile(__dirname+'/static_content/fullcalendar.js')
-        .catch( function(err)  {
-            console.log(err)
-    });
 }
