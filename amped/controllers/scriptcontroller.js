2ac50156c60e44044e5d4ce1fbe8f84ca28bfba8
Creich21 <creich21@student.aau.dk>
2022-05-23 19:12:28 +0200

    .catch

--- amped/controllers/scriptcontroller.js
+++ amped/controllers/scriptcontroller.js
@@ -3,15 +3,14 @@ const Fullcalendar = require('fullcalendar');
 
 exports.fullcalendarcss =  function (req, res, next) {
         res.sendFile(__dirname+'/static_content/fullcalendar.css')
-        // .catch( function(err)  {
-        //     console.log(err)
-        // });
+        .catch( function(err)  {
+            console.log(err)
+        });
 }
 
 exports.fullcalendarjs =  function (req, res, next) {
         res.sendFile(__dirname+'/static_content/fullcalendar.js')
-        //, {eventlist: data }, {calendermodule: Fullcalendar});
-        // .catch( function(err)  {
-        //     console.log(err)
-    // });
+        .catch( function(err)  {
+            console.log(err)
+    });
 }
