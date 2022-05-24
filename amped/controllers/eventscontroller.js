9652725c4dc794797f99890a69f4d613bff0afb3
Casper2910 <casperbramm@hotmail.com>
2022-05-24 15:26:35 +0200

    comments

--- amped/controllers/eventscontroller.js
+++ amped/controllers/eventscontroller.js
@@ -67,8 +67,9 @@ exports.createevent = (req, res, next) => {
     res.render("createevent")
 }
 
-// opret event
+// opret event 
 exports.createevents = (req, res, next) => {
+    //tjekker for om pris er Null og sætter den til 0 hvis ja
     var pris = req.body.pris
 
     if (pris.length == 0){
@@ -77,7 +78,7 @@ exports.createevents = (req, res, next) => {
     }else{
         pris = req.body.pris
     }
-
+    //laver en event med givne data indsat i vores forms
     Events.create({
         group_id : req.session.Group.id,
         event_name: req.body.event_name,
@@ -92,18 +93,23 @@ exports.createevents = (req, res, next) => {
 
     }).then(function (event) {
         if (event) {
+            //hvis eventen blev kreeret uden fejl bliver brugeren sendt
+            //tilbage til grouppanelet
             res.redirect('/grouppanel');
         } else {
             response.status(400).send('Error in insert new event');
         }
     });
 }
+
 exports.findOne = (req, res) => {
+    //finder id for eventet og gemmer det i idcheck
     const idcheck = req.query.id
-    console.log(idcheck)
+    //finder den event der er blevet valgt ud fra id'et
     Events.findAll({where: {'id' : { [Op.eq]: idcheck} }})
 
     .then(function(data) {
+        //render eventinfo.pug med informationer om den givne event
         res.render('eventinfo', {eventlist: data },
         console.log(data));
     })
@@ -113,17 +119,19 @@ exports.findOne = (req, res) => {
 };
 // events delete for admin
 exports.eventsdelete = function(req, res, next){
+    //chekker om brugeren i session er admin
     if (req.session.loggedIn !== true || req.session.Group.roles < 2){
         res.render("nopugmission")
         return
     }
+    //gemmer vores id i idcheck
     const idcheck = req.query.id
-    console.log(idcheck)
     return Events.destroy({
         //slet ud fra id
         where: {'id' : { [Op.eq]: idcheck} }
     }).then(function (event) {
         if (event) {
+            //redirecter tilbahe til admin/events
             res.redirect('/admin/events');
         } else {
             res.status(400).send('Error in delete');
@@ -144,15 +152,16 @@ exports.updateeventsadmin = (req, res, next) => {
 }
 // opdater event for admin event
 exports.eventsupdate = function (req, res, next) {
+    //gemmer det valgte id i idcheck
     const idcheck = req.body.id
-    console.log(idcheck)
+    //chekker om brugeren er admin
     if (req.session.loggedIn !== true || req.session.Group.roles < 2) {
         res.render("nopugmission")
         return
     }
 
     Events.update(
-        // Values to update
+        // Values to update (id erstattes med det samme id)
         {
             id:idcheck,
             event_name: req.body.event_name,
@@ -163,11 +172,12 @@ exports.eventsupdate = function (req, res, next) {
             pris: req.body.pris,
             fburl: req.body.fburl,
         },
-        {
+        {   //opdatere ud fra valgte id
             where: {'id': {[Op.eq]: idcheck} }
         }
     ).then(function (groups) {
         if (groups) {
+            //redirecter til admins/events
             res.redirect('/admin/events');
         } else {
             response.status(400).send('Error in update');
@@ -177,10 +187,12 @@ exports.eventsupdate = function (req, res, next) {
 
 // viser update event page for grouppanel
 exports.updateeventsgroup = (req, res, next) => {
+    //gemmer event id'et i idcheck
     const idcheck = req.query.id
-    console.log(idcheck)
+    //finder den event med samme id
     Events.findAll({where: {'id': {[Op.eq]: idcheck}}})
         .then(function (data) {
+            //render den info ud til grouppanelupdate.pug
             res.render('grouppanelupdate', {eventlist: data},
                 console.log(data));
 
@@ -189,12 +201,14 @@ exports.updateeventsgroup = (req, res, next) => {
 
 // opdater event for grouppanel
 exports.eventsupdategroup = function (req, res, next) {
+    //gemmer det valgte id i idcheck
     const idcheck = req.body.id
-    console.log(idcheck)
+    //chekker om gruppen der er i session har role 1 eller over
     if (req.session.loggedIn !== true || req.session.Group.roles < 1) {
         res.render("nopugmission")
         return
     }
+    //hvis pris er null sættes pris til : 0
     var pris = req.body.pris
     if (pris.length == 0){
         pris = 0;
@@ -215,14 +229,17 @@ exports.eventsupdategroup = function (req, res, next) {
             pris: pris,
             fburl: req.body.fburl,
         },
-        {
+        { //opdatere eventen der har det samme id som vores idcheck
             where:{
             [Op.and]: [ 
             {id: {[Op.eq]: idcheck}},
+            //checker om den gruppe der skal opdateres tilhører
+            //den gruppe som er i session
             {group_id: req.session.Group.id}
         ]}}
     ).then(function (groups) {
         if (groups) {
+            //redirecter til grouppanel.pug
             res.redirect('/grouppanel');
         } else {
             response.status(400).send('Error in update');
@@ -232,20 +249,24 @@ exports.eventsupdategroup = function (req, res, next) {
 
 // event delete for group
 exports.eventsdeletegroup = function(req, res, next){
+    //chekker om gruppen i session har role 1 eller over
     if (req.session.loggedIn !== true || req.session.Group.roles < 1){
         res.render("nopugmission")
         return
     }
+    //gemmer id'et i idcheck
     const idcheck = req.query.id
-    console.log(idcheck)
         Events.destroy({ where:{ 
-            //slet ud fra id og hvor group_id er det samme som det id der er i session
+            //slet ud fra id
             [Op.and]: [
             { id : { [Op.eq]: idcheck}},
+            //checker om den gruppe der skal slettes tilhører
+            //den gruppe som er i session
             { group_id: req.session.Group.id}]
             }
         }).then(function (event) {
             if (event) {
+                //redirecter til grouppanel.pug
                 res.redirect('/grouppanel');
             } else {
                 res.status(400).send('Error in delete');
