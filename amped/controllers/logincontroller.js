c320c833de5e5da7fa6a18c6bb2c4e1041a71ea9
Shayaan260 <50516898+Shayaan260@users.noreply.github.com>
2022-05-24 14:45:34 +0200

    h

--- amped/controllers/logincontroller.js
+++ amped/controllers/logincontroller.js
@@ -1,24 +1,24 @@
-const Groups = require("../models/groups");
+const Groups = require("../models/groups"); //Her henter vi "Groups"-modellen fra "/models/groups".
 
-exports.loginpage = (req, res, next) => {
-    if(req.session.loggedIn) {
-        res.redirect("/events")
+exports.loginpage = (req, res, next) => { //her exporteres controlleren "loginpage".
+    if(req.session.loggedIn) { //her er et if statement som tjekker om session.loggedIn eksistere, dette er tilfældet ved login.
+        res.redirect("/events") // Hvis session.loggedIn eksistere, altså man allerede er logged in så redirectes man til "events" siden.
         return
     }
-    res.render('login')
+    res.render('login') // Hvis session.loggedIn ikke eksistere, ergo man ikke er logged in, føres man til login siden.
 }
 
-exports.loggingin = (req, res, next) => {
-    const Username = req.body.Username
-    const Password = req.body.password
-    if(!Username || !Password ){
-         res.send("Username and/or Password is incorrect") 
+exports.loggingin = (req, res, next) => { //her exporteres controlleren "loggingin".
+    const Username = req.body.Username // Teksten hentes fra feltet username på login siden.
+    const Password = req.body.password // Teksten hentes fra feltet password på login siden.
+    if(!Username || !Password ){ // et if statement som tjekker om Username eller Password ikke eksisterer.
+         res.send("Username and/or Password is incorrect") // hvis Username og Password ikke eksistere sendes denne respons til brugeren.
         return
     }
 
-    Groups.findOne({where: {group_name : Username}})
-    .then( function(Group){
-        if (Group === null){
+    Groups.findOne({where: {group_name : Username}}) //Her bruges sequelize til at finde en bruger med dette Username i databasen ved hjælp af et objekt fra "Groups"-modellen, group_name.
+    .then( function(Group){ //derefter tager den dette group_name og finder alt informationen fra dens group i forhold til "Groups"-modellen
+        if (Group === null){ //Hvis Username
             res.send("Username and/or Password is incorrect")
             return
         }
