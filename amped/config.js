8a3795cc4a682828d97b04ca009a9a74d4a4a439
Casper2910 <casperbramm@hotmail.com>
2022-05-24 17:09:34 +0200

    config comments

--- amped/config.js
+++ amped/config.js
@@ -2,9 +2,13 @@ const Sequelize = require("sequelize");
 //Op operator functionality
 const {Datatypes, Op} = Sequelize;
 
+//connectio til vores database, EventMan er navnet på databasen, remote er vores username og P2gruppe#mote er vores password
 const connection = new Sequelize("EventMan", "remote", "P2grupper#mote",{
+    //specificere hvilken slags database vi bruger
     dialect: "mysql",
+    //ip for host
     host: "10.92.0.104",
+    //vi vil ikke have timestamps, og vi vil gerne fryse tablen når vi henter den
     define: {
         timestamps: false,
         freezeTableName: true,
